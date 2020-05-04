31bfe7c1ccb0f19c80f8cb460833bb1b0cb9c1c2
Marcin Herda <mherda@gmail.com>
2020-05-04 01:16:02 +0100

    Updates

---
+++ precache-manifest.4e5920177b47c684dcefc67015a865da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845f2c8728099033f7e9f2ded9d89593",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "e7b9137f25e803c8b069",
+    "url": "/monsters/static/css/main.9418a0af.chunk.css"
+  },
+  {
+    "revision": "bf7571f19fcec1cbcead",
+    "url": "/monsters/static/js/2.4a190e57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.4a190e57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b9137f25e803c8b069",
+    "url": "/monsters/static/js/main.53d36b22.chunk.js"
+  },
+  {
+    "revision": "ced4be958685c21c8a7d",
+    "url": "/monsters/static/js/runtime-main.00caf551.js"
+  }
+]);
